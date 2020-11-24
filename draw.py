410d3dfa147526ad50d1e2f01015590b1247620c
saya9729 <ductaiprono1@gmail.com>
2020-11-24 12:08:00 +0700

    fix overwrite

--- draw.py
+++ draw.py
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import world
-
-
-
-
-# input data-------------------------
-file = open("map1.txt", "r")
-line = file.readlines()
-n = int(line[0].split()[0])
-m = int(line[0].split()[1])
-map = [[0 for i in range(m)] for j in range(n)]
-for i in range(n):
-    for j in range(m):
-        map[i][j] = int(line[i + 1].split()[j])
-
-global_mutation_rate=0.01
-vision_radius=3
-
-World=world.world(5,2000)
-
-def draw():
-    draw_data()
-
-    if not World.done():
-        World.update()
-    else:
-        World.genetic_algorithm()
-
-def draw_data():
-    #minh xu ly ho
-=======
 import pygame
 import math
 import time
@@ -156,4 +124,3 @@ while running:
     hider(hider_x, hider_y)
     # time.sleep(0.1)
     pygame.display.update()
->>>>>>> 0430660fe718c6b3233b3ab62e883dbfd6eefbaa
